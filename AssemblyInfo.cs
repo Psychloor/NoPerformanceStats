54bca843d1deaf6ce5ed03a931bb8e6e56a2bfb3
Psychloor <blomqvistkevin@hotmail.com>
2021-05-20 22:09:18 +0200

    Updated to ML 0.3.0 and changed to Native Patches

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,25 +1,28 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
+
 using MelonLoader;
 
-[assembly: AssemblyTitle(NoPerformanceStats.BuildInfo.Name)]
+using BuildInfo = NoPerformanceStats.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.Name)]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany(NoPerformanceStats.BuildInfo.Company)]
-[assembly: AssemblyProduct(NoPerformanceStats.BuildInfo.Name)]
-[assembly: AssemblyCopyright("Created by " + NoPerformanceStats.BuildInfo.Author)]
-[assembly: AssemblyTrademark(NoPerformanceStats.BuildInfo.Company)]
+[assembly: AssemblyCompany(BuildInfo.Company)]
+[assembly: AssemblyProduct(BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + BuildInfo.Author)]
+[assembly: AssemblyTrademark(BuildInfo.Company)]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-//[assembly: Guid("")]
-[assembly: AssemblyVersion(NoPerformanceStats.BuildInfo.Version)]
-[assembly: AssemblyFileVersion(NoPerformanceStats.BuildInfo.Version)]
-[assembly: NeutralResourcesLanguage("en")]
-[assembly: MelonModInfo(typeof(NoPerformanceStats.NoPerformanceStats), NoPerformanceStats.BuildInfo.Name, NoPerformanceStats.BuildInfo.Version, NoPerformanceStats.BuildInfo.Author, NoPerformanceStats.BuildInfo.DownloadLink)]
 
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(BuildInfo.Version)]
+[assembly: AssemblyFileVersion(BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(NoPerformanceStats.NoPerformanceStats), BuildInfo.Name, BuildInfo.Version, BuildInfo.Author, BuildInfo.DownloadLink)]
 
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonModGame("VRChat", "VRChat")]
+[assembly: MelonGame("VRChat", "VRChat")]
--- NoPerformanceStats.cs
+++ NoPerformanceStats.cs
@@ -1,65 +1,146 @@
-﻿using Harmony;
-using MelonLoader;
-using System;
-using System.Reflection;
-using UnityEngine;
-using VRCSDK2.Validation.Performance;
-
-namespace NoPerformanceStats
+﻿namespace NoPerformanceStats
 {
+
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+
+    using Il2CppSystem.Collections;
+
+    using MelonLoader;
+
+    using UnhollowerBaseLib;
+
+    using UnityEngine;
+
+    using VRCSDK2.Validation.Performance;
 
     public static class BuildInfo
     {
+
         public const string Name = "NoPerformanceStats";
-        public const string Author = "ImTiara";
+
+        public const string Author = "ImTiara & Psychloor";
+
         public const string Company = null;
-        public const string Version = "1.0.2";
-        public const string DownloadLink = "https://github.com/ImTiara/NoPerformanceStats/releases";
+
+        public const string Version = "1.1.0";
+
+        public const string DownloadLink = "https://github.com/Psychloor/NoPerformanceStats/";
+
     }
 
     public class NoPerformanceStats : MelonMod
     {
 
-        private HarmonyInstance harmonyInstance;
-        private static bool allowPerformanceScanner;
+        private static MelonPreferences_Entry<bool> disablePerformanceStatsEntry;
+
+        private static CalculatePerformanceStats calculatePerformanceStats;
+
+        private static CalculatePerformanceStatsEnumerator calculatePerformanceStatsEnumerator;
+
+        private static ApplyPerformanceFiltersEnumerator applyPerformanceFiltersEnumerator;
+
+        private MelonPreferences_Category settingsCategory;
+
+        private static unsafe TDelegate Patch<TDelegate>(MethodBase originalMethod, IntPtr patchDetour)
+        {
+            IntPtr original = *(IntPtr*)(IntPtr)UnhollowerUtils.GetIl2CppMethodInfoPointerFieldForGeneratedMethod(originalMethod).GetValue(null);
+            MelonUtils.NativeHookAttach((IntPtr)(&original), patchDetour);
+            return Marshal.GetDelegateForFunctionPointer<TDelegate>(original);
+        }
+
+        private static IntPtr GetDetour(string name)
+        {
+            return typeof(NoPerformanceStats).GetMethod(name, BindingFlags.NonPublic | BindingFlags.Static)!.MethodHandle.GetFunctionPointer();
+        }
 
         public override void OnApplicationStart()
         {
-            ModPrefs.RegisterCategory("NoPerformanceStats", "No Performance Stats");
-            ModPrefs.RegisterPrefBool("NoPerformanceStats", "DisablePerformanceStats", true, "Disable Performance Stats");
+            settingsCategory = MelonPreferences.CreateCategory("NoPerformanceStats", "No Performance Stats");
+            disablePerformanceStatsEntry =
+                settingsCategory.CreateEntry("DisablePerformanceStats", true, "Disable Performance Stats") as MelonPreferences_Entry<bool>;
 
-            LoadModPrefs();
+            MethodInfo calculatePerformanceStatsMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static).First(
+                m => m.Name.StartsWith("Method_Public_Static_Void_String_GameObject_AvatarPerformanceStats_")
+                     && m.ReturnType == typeof(void)
+                     && m.GetParameters().Length == 3);
+            calculatePerformanceStats = Patch<CalculatePerformanceStats>(calculatePerformanceStatsMethod, GetDetour(nameof(CalculatePerformanceStatsPatch)));
 
-            harmonyInstance = HarmonyInstance.Create("NoPerformanceStatsPatcher");
+            MethodInfo calculatePerformanceStatsEnumeratorMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static).First(
+                m => m.Name.StartsWith("Method_Public_Static_IEnumerator_String_GameObject_AvatarPerformanceStats") && m.GetParameters().Length == 3);
+            calculatePerformanceStatsEnumerator = Patch<CalculatePerformanceStatsEnumerator>(
+                calculatePerformanceStatsEnumeratorMethod,
+                GetDetour(nameof(CalculatePerformanceStatsEnumeratorPatch)));
 
-            try
-            {
-                MethodInfo[] methods = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static);
-                for (int i = 0; i < methods.Length; i++)
-                    if (methods[i].Name == "Method_Public_Static_IEnumerator_String_GameObject_AvatarPerformanceStats_0" || methods[i].Name == "Method_Public_Static_IEnumerator_GameObject_AvatarPerformanceStats_EnumPublicSealedvaNoExGoMePoVe7vUnique_MulticastDelegateNPublicSealedVoUnique_0" || methods[i].Name == "Method_Public_Static_Void_String_GameObject_AvatarPerformanceStats_0")
-                        harmonyInstance.Patch(methods[i], new HarmonyMethod(typeof(NoPerformanceStats).GetMethod("CalculatePerformance", BindingFlags.Static | BindingFlags.NonPublic)), null, null);
-            }
-            catch(Exception e)
-            {
-                MelonModLogger.Log(ConsoleColor.Red, "Failed to patch Performance Scanners: " + e);
-            }
+            MethodInfo applyPerformanceFiltersMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static).First(
+                m => m.Name.StartsWith("Method_Public_Static_IEnumerator_GameObject_AvatarPerformanceStats_PerformanceRating_MulticastDelegate")
+                     && m.GetParameters().Length == 4);
+            applyPerformanceFiltersEnumerator = Patch<ApplyPerformanceFiltersEnumerator>(
+                applyPerformanceFiltersMethod,
+                GetDetour(nameof(ApplyPerformanceFiltersPatch)));
         }
 
         public override void OnUpdate()
         {
-            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.P))
+            if (Input.GetKeyDown(KeyCode.P)
+                && Input.GetKey(KeyCode.LeftShift)
+                && Input.GetKey(KeyCode.LeftControl))
             {
-                ModPrefs.SetBool("NoPerformanceStats", "DisablePerformanceStats", allowPerformanceScanner);
-                LoadModPrefs();
-                MelonModLogger.Log("Avatar Performance Stats is now " + (allowPerformanceScanner ? "ENABLED" : "DISABLED"));
+                disablePerformanceStatsEntry.Value = !disablePerformanceStatsEntry.Value;
+                disablePerformanceStatsEntry.Save();
+
+                MelonLogger.Msg("Avatar Performance Stats is now " + (!disablePerformanceStatsEntry.Value ? "ENABLED" : "DISABLED"));
             }
         }
 
-        public override void OnModSettingsApplied() => LoadModPrefs();
+        private static IEnumerator ApplyPerformanceFiltersPatch(
+            IntPtr avatarObjectPtr,
+            IntPtr performanceStatsPtr,
+            int minimumPerformanceRating,
+            IntPtr onBlockPtr,
+            IntPtr stackPtr)
+        {
+            return disablePerformanceStatsEntry.Value
+                       ? null
+                       : applyPerformanceFiltersEnumerator(avatarObjectPtr, performanceStatsPtr, minimumPerformanceRating, onBlockPtr, stackPtr);
+        }
 
-        private void LoadModPrefs() => allowPerformanceScanner = !ModPrefs.GetBool("NoPerformanceStats", "DisablePerformanceStats");
+        private static IEnumerator CalculatePerformanceStatsEnumeratorPatch(
+            IntPtr avatarNamePtr,
+            IntPtr avatarObjectPtr,
+            IntPtr performanceStatsPtr,
+            IntPtr stackPtr)
+        {
+            return disablePerformanceStatsEntry.Value
+                       ? null
+                       : calculatePerformanceStatsEnumerator(avatarNamePtr, avatarObjectPtr, performanceStatsPtr, stackPtr);
+        }
 
-        private static bool CalculatePerformance() => allowPerformanceScanner;
+        private static void CalculatePerformanceStatsPatch(IntPtr avatarNamePtr, IntPtr avatarObjectPtr, IntPtr performanceStatsPtr, IntPtr stackPtr)
+        {
+            if (disablePerformanceStatsEntry.Value) return;
+            calculatePerformanceStats(avatarNamePtr, avatarObjectPtr, performanceStatsPtr, stackPtr);
+        }
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void CalculatePerformanceStats(IntPtr avatarNamePtr, IntPtr avatarObjectPtr, IntPtr performanceStatsPtr, IntPtr stackPtr);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IEnumerator CalculatePerformanceStatsEnumerator(
+            IntPtr avatarNamePtr,
+            IntPtr avatarObjectPtr,
+            IntPtr performanceStatsPtr,
+            IntPtr stackPtr);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IEnumerator ApplyPerformanceFiltersEnumerator(
+            IntPtr avatarObjectPtr,
+            IntPtr performanceStatsPtr,
+            int minimumPerformanceRating,
+            IntPtr onBlockPtr,
+            IntPtr stackPtr);
 
     }
 
