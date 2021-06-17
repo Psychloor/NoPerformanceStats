5ec737a7877038bb6ca699728489f0f6b04a71ba
Psychloor <blomqvistkevin@hotmail.com>
2021-06-17 18:32:56 +0200

    Tiara's Update to native

--- NoPerformanceStats.cs
+++ NoPerformanceStats.cs
@@ -2,13 +2,15 @@
 {
 
     using System;
-    using System.Linq;
     using System.Reflection;
     using System.Runtime.InteropServices;
 
     using MelonLoader;
 
     using UnityEngine;
+
+    using VRC.SDKBase.Validation.Performance;
+    using VRC.SDKBase.Validation.Performance.Stats;
 
     using VRCSDK2.Validation.Performance;
 
@@ -21,22 +23,18 @@
 
         public const string Company = null;
 
-        public const string Version = "1.1.1";
+        public const string Version = "1.1.2";
 
         public const string DownloadLink = "https://github.com/Psychloor/NoPerformanceStats/";
 
     }
 
-    public class NoPerformanceStats : MelonMod
+    public sealed class NoPerformanceStats : MelonMod
     {
 
         private static MelonPreferences_Entry<bool> disablePerformanceStatsEntry;
 
-        private static CalculatePerformanceStats calculatePerformanceStats;
-
-        private static CalculatePerformanceStatsEnumerator calculatePerformanceStatsEnumerator;
-
-        private static ApplyPerformanceFiltersEnumerator applyPerformanceFiltersEnumerator;
+        private static CalculatePerformanceDelegate calculatePerformanceDelegate;
 
         private MelonPreferences_Category settingsCategory;
 
@@ -55,84 +53,32 @@
         public override void OnApplicationStart()
         {
             settingsCategory = MelonPreferences.CreateCategory("NoPerformanceStats", "No Performance Stats");
-            disablePerformanceStatsEntry =
-                settingsCategory.CreateEntry("DisablePerformanceStats", true, "Disable Performance Stats");
+            disablePerformanceStatsEntry = settingsCategory.CreateEntry("DisablePerformanceStats", true, "Disable Performance Stats");
 
-            MethodInfo calculatePerformanceStatsMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly).Single(
-                m => m.Name.StartsWith("Method_Public_Static_Void_String_GameObject_AvatarPerformanceStats_")
-                     && m.ReturnType == typeof(void)
-                     && m.GetParameters().Length == 3);
-            calculatePerformanceStats = Patch<CalculatePerformanceStats>(calculatePerformanceStatsMethod, GetDetour(nameof(CalculatePerformanceStatsPatch)));
-
-            MethodInfo calculatePerformanceStatsEnumeratorMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly).Single(
-                m => m.Name.StartsWith("Method_Public_Static_IEnumerator_String_GameObject_AvatarPerformanceStats") && m.GetParameters().Length == 3);
-            calculatePerformanceStatsEnumerator = Patch<CalculatePerformanceStatsEnumerator>(
-                calculatePerformanceStatsEnumeratorMethod,
-                GetDetour(nameof(CalculatePerformanceStatsEnumeratorPatch)));
-
-            MethodInfo applyPerformanceFiltersMethod = typeof(AvatarPerformance).GetMethods(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly).Single(
-                m => m.Name.StartsWith("Method_Public_Static_IEnumerator_GameObject_AvatarPerformanceStats_PerformanceRating_MulticastDelegate")
-                     && m.GetParameters().Length == 4);
-            applyPerformanceFiltersEnumerator = Patch<ApplyPerformanceFiltersEnumerator>(
-                applyPerformanceFiltersMethod,
-                GetDetour(nameof(ApplyPerformanceFiltersPatch)));
-        }
-
-        public override void OnUpdate()
-        {
-            if (Input.GetKeyDown(KeyCode.P)
-                && Input.GetKey(KeyCode.LeftShift)
-                && Input.GetKey(KeyCode.LeftControl))
+            try
             {
-                disablePerformanceStatsEntry.Value = !disablePerformanceStatsEntry.Value;
-                disablePerformanceStatsEntry.Save();
+                MethodInfo calculateEnumerator = typeof(PerformanceScannerSet).GetMethod(
+                    nameof(PerformanceScannerSet.Method_Public_IEnumerator_GameObject_AvatarPerformanceStats_MulticastDelegateNPublicSealedBoCoUnique_0),
+                    BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly,
+                    null,
+                    new[] { typeof(GameObject), typeof(AvatarPerformanceStats), typeof(AvatarPerformance.MulticastDelegateNPublicSealedBoCoUnique) },
+                    null);
 
-                MelonLogger.Msg("Avatar Performance Stats is now " + (!disablePerformanceStatsEntry.Value ? "ENABLED" : "DISABLED"));
+                calculatePerformanceDelegate = Patch<CalculatePerformanceDelegate>(calculateEnumerator, GetDetour(nameof(CalculatePerformancePatch)));
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error("Failed to patch CalculatePerformanceStats\n" + e);
             }
         }
 
-        private static IntPtr ApplyPerformanceFiltersPatch(
-            IntPtr avatarObjectPtr,
-            IntPtr performanceStatsPtr,
-            int minimumPerformanceRating,
-            IntPtr onBlockPtr,
-            IntPtr stackPtr)
+        private static IntPtr CalculatePerformancePatch(IntPtr instancePtr, IntPtr gameObjectPtr, IntPtr statsPtr, IntPtr delegatePtr, IntPtr stackPtr)
         {
-            return disablePerformanceStatsEntry.Value
-                       ? IntPtr.Zero
-                       : applyPerformanceFiltersEnumerator(avatarObjectPtr, performanceStatsPtr, minimumPerformanceRating, onBlockPtr, stackPtr);
-        }
-
-        private static IntPtr CalculatePerformanceStatsEnumeratorPatch(
-            IntPtr avatarNamePtr,
-            IntPtr avatarObjectPtr,
-            IntPtr performanceStatsPtr,
-            IntPtr stackPtr)
-        {
-            return disablePerformanceStatsEntry.Value
-                       ? IntPtr.Zero
-                       : calculatePerformanceStatsEnumerator(avatarNamePtr, avatarObjectPtr, performanceStatsPtr, stackPtr);
-        }
-
-        private static void CalculatePerformanceStatsPatch(IntPtr avatarNamePtr, IntPtr avatarObjectPtr, IntPtr performanceStatsPtr, IntPtr stackPtr)
-        {
-            if (disablePerformanceStatsEntry.Value) return;
-            calculatePerformanceStats(avatarNamePtr, avatarObjectPtr, performanceStatsPtr, stackPtr);
+            return disablePerformanceStatsEntry.Value ? IntPtr.Zero : calculatePerformanceDelegate(instancePtr, gameObjectPtr, statsPtr, delegatePtr, stackPtr);
         }
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate void CalculatePerformanceStats(IntPtr avatarNamePtr, IntPtr avatarObjectPtr, IntPtr performanceStatsPtr, IntPtr stackPtr);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr CalculatePerformanceStatsEnumerator(IntPtr avatarNamePtr, IntPtr avatarObjectPtr, IntPtr performanceStatsPtr, IntPtr stackPtr);
-
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-        private delegate IntPtr ApplyPerformanceFiltersEnumerator(
-            IntPtr avatarObjectPtr,
-            IntPtr performanceStatsPtr,
-            int minimumPerformanceRating,
-            IntPtr onBlockPtr,
-            IntPtr stackPtr);
+        private delegate IntPtr CalculatePerformanceDelegate(IntPtr instancePtr, IntPtr gameObjectPtr, IntPtr statsPtr, IntPtr delegatePtr, IntPtr stackPtr);
 
     }
 
